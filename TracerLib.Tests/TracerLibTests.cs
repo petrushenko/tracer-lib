24e10307acb8ec5a95472f65ef1ba3d23d491e0e
petrushenko <igor.petrushenko1@gmail.com>
2019-09-18 16:30:39 +0300

    Added unit tests project

---
+++ TracerLib.Tests/TracerLibTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TracerLib.Tests
+{
+    [TestClass]
+    public class TracerLibTests
+    {
+
+        public Tracer Tracer = new Tracer();
+
+        List<Thread> Threads = new List<Thread>();
+
+        int ThreadsCount = 5;
+
+        int MillisecondsTimeout = 100;
+
+        private void Method()
+        {
+            Tracer.StartTrace();
+            Thread.Sleep(MillisecondsTimeout);
+            Tracer.StopTrace();
+
+        }
+
+        [TestMethod]
+        public void ExecutionTime()
+        {
+            Method();
+            TraceResult traceResult = Tracer.GetTraceResult();
+            double methodTime = traceResult.ThreadsInfo[0].Methods[0].ExecutionTime;
+            double threadTime = traceResult.ThreadsInfo[0].ExecutionTime;
+            Assert.IsTrue(methodTime >= MillisecondsTimeout);
+            Assert.IsTrue(threadTime >= MillisecondsTimeout);
+        }
+
+        [TestMethod]
+        public void ThreadCount()
+        {
+            //creats (ThreadsCount - 1) thread
+            for (int i = 0; i < ThreadsCount; i++)
+            {
+                Threads.Add(new Thread(Method));
+            }
+
+            foreach (Thread thread in Threads)
+            {
+                thread.Start();
+                thread.Join();
+            }
+
+            TraceResult traceResult = Tracer.GetTraceResult();
+            // (ThreadsCount - 1) and one main thread
+            Assert.AreEqual(ThreadsCount, traceResult.ThreadsInfo.Count);
+        }
+
+
+
+    }
+}

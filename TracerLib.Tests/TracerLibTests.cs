5152f1173e5522cd3dcc417719bec7d36174d502
petrushenko <igor.petrushenko1@gmail.com>
2019-09-18 18:22:56 +0300

    Added new tests

--- TracerLib.Tests/TracerLibTests.cs
+++ TracerLib.Tests/TracerLibTests.cs
@@ -14,6 +14,7 @@ namespace TracerLib.Tests
         List<Thread> Threads = new List<Thread>();
 
         int ThreadsCount = 5;
+        int MethodsCount = 5;
 
         int MillisecondsTimeout = 100;
 
@@ -22,7 +23,22 @@ namespace TracerLib.Tests
             Tracer.StartTrace();
             Thread.Sleep(MillisecondsTimeout);
             Tracer.StopTrace();
+        }
 
+        private void Method1()
+        {
+            Tracer.StartTrace();
+            for (int i = 0; i < ThreadsCount; i++)
+            {
+                Threads.Add(new Thread(Method));
+            }
+
+            foreach (Thread thread in Threads)
+            {
+                thread.Start();
+                thread.Join();
+            }
+            Tracer.StopTrace();
         }
 
         [TestMethod]
@@ -39,7 +55,6 @@ namespace TracerLib.Tests
         [TestMethod]
         public void ThreadCount()
         {
-            //creats (ThreadsCount - 1) thread
             for (int i = 0; i < ThreadsCount; i++)
             {
                 Threads.Add(new Thread(Method));
@@ -52,11 +67,47 @@ namespace TracerLib.Tests
             }
 
             TraceResult traceResult = Tracer.GetTraceResult();
-            // (ThreadsCount - 1) and one main thread
             Assert.AreEqual(ThreadsCount, traceResult.ThreadsInfo.Count);
         }
 
+        [TestMethod]
+        public void MethodCount()
+        {
+            for (int i = 0; i < MethodsCount; i++)
+            {
+                Method();
+            }
 
+            TraceResult traceResult = Tracer.GetTraceResult();
+            Assert.AreEqual(MethodsCount, traceResult.ThreadsInfo[0].Methods.Count);
+        }
 
+        [TestMethod]
+        public void ThreadsInMethod()
+        {
+            //creates {ThreadsCount} threads
+            for (int i = 0; i < ThreadsCount; i++)
+            {
+                Threads.Add(new Thread(Method));
+            }
+            foreach (Thread thread in Threads)
+            {
+                thread.Start();
+                thread.Join();
+            }
+            TraceResult traceResult = Tracer.GetTraceResult();
+            Assert.AreEqual(ThreadsCount, traceResult.ThreadsInfo.Count);
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            Tracer.StartTrace();
+            Tracer.StopTrace();
+            TraceResult traceResult = Tracer.GetTraceResult();
+            Assert.AreEqual(nameof(Name), traceResult.ThreadsInfo[0].Methods[0].Name);
+            Assert.AreEqual(nameof(TracerLibTests), traceResult.ThreadsInfo[0].Methods[0].ClassName);
+            Assert.AreEqual(Thread.CurrentThread.ManagedThreadId, traceResult.ThreadsInfo[0].Id);
+        }
     }
 }

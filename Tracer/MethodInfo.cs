380c2dd454b3c8ecb9b0403d721f7987c215359e
petrushenko <igor.petrushenko1@gmail.com>
2019-09-18 14:09:48 +0300

    Fixed error in tracing inner methods

---
+++ Tracer/MethodInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Tracer
+{
+    public class MethodInfo
+    {
+        public Stopwatch Stopwatch = new Stopwatch();
+        public string Name { get; set; }
+
+        public string ClassName { get; set; }
+
+        public double ExecutionTime { get; set; }
+
+        public List<MethodInfo> ChildMethods = new List<MethodInfo>();
+    }
+}
---
+++ Tracer/ThreadInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public class ThreadInfo
+    {
+        public string Id { get; set; }
+        public double ExecutionTime { get; set; }
+
+        public List<MethodInfo> Methods = new List<MethodInfo>();
+    }
+}

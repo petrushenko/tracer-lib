40edc03a14e6b38709d81694c30021445dfe4ed6
petrushenko <igor.petrushenko1@gmail.com>
2019-09-18 14:51:27 +0300

    Added XML serialization

--- TracerLib/XmlTracerSerializer.cs
+++ TracerLib/XmlTracerSerializer.cs
@@ -1,11 +1,7 @@
-﻿using System;
+﻿using System.Xml.Linq;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.Serialization;
 using System.IO;
-using System.Xml.Serialization;
+using System.Xml;
 
 namespace TracerLib
 {
@@ -13,12 +9,65 @@ namespace TracerLib
     {
         public string Serealize(TraceResult traceResult)
         {
-            var aSerializer = new XmlSerializer(typeof(TraceResult));
-            StringBuilder sb = new StringBuilder();
-            StringWriter sw = new StringWriter(sb);
-            aSerializer.Serialize(sw, traceResult); // pass an instance of A
-            string xmlResult = sw.GetStringBuilder().ToString();
-            return xmlResult;
+            List<ThreadInfo> threadsInfo = traceResult.ThreadsInfo;
+            XDocument xDocument = new XDocument(new XElement("root"));
+
+            foreach (ThreadInfo thread in threadsInfo)
+            {
+                XElement threadXElement = GetThreadXElement(thread);
+                xDocument.Root.Add(threadXElement);
+            }
+
+            StringWriter stringWriter = new StringWriter();
+            using (XmlTextWriter xmlWriter = new XmlTextWriter(stringWriter))
+            {
+                xmlWriter.Formatting = Formatting.Indented;
+                xDocument.WriteTo(xmlWriter);
+            }
+            return stringWriter.ToString();
         }
+
+        private XElement GetMethodXElement(MethodInfo methodInfo)
+        {
+            return new XElement(
+                "method",
+                new XAttribute( "name", methodInfo.Name ),
+                new XAttribute( "class", methodInfo.ClassName),
+                new XAttribute( "time", methodInfo.ExecutionTime)
+                );
+        }
+
+        private XElement GetMethodXElementWithChildMethods(MethodInfo methodInfo)
+        {
+            XElement methodXElement = GetMethodXElement(methodInfo);
+            foreach (MethodInfo method in methodInfo.ChildMethods)
+            {
+                XElement childMethod = GetMethodXElement(method);
+                if (method.ChildMethods.Count > 0)
+                {
+                    childMethod = GetMethodXElementWithChildMethods(method);
+                }
+                methodXElement.Add(childMethod);
+            }
+            return methodXElement;
+        }
+
+        private XElement GetThreadXElement(ThreadInfo threadInfo)
+        {
+            XElement threadXElement = new XElement(
+                "thread",
+                new XAttribute("id", threadInfo.Id),
+                new XAttribute("time", threadInfo.ExecutionTime)
+                );
+            foreach (MethodInfo method in threadInfo.Methods)
+            {
+                XElement methodXElement = GetMethodXElementWithChildMethods(method);
+                threadXElement.Add(methodXElement);
+            }
+            return threadXElement;
+        }
+
+
+
     }
 }
